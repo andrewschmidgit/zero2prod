02de6f90360cf1d35b9b57f2cafb631f6f64986f
Andrew Schmid <andrewschmid-dev@proton.me>
2023-11-16 21:30:45 -0600

    Added axum + tokio, basic greet route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Path, Router, routing::get};
+
+async fn greet(path: Option<Path<String>>) -> String {
+    let name = match path {
+        Some(Path(name)) => name,
+        None => "World".to_string(),
+    };
+
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:name", get(greet));
+
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
