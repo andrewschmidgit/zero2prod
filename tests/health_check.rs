d72472d3a8921f5c3152d34a0eacc61b0cef4163
Andrew Schmid <andrewschmid-dev@proton.me>
2023-11-18 13:08:59 -0600

    Now binds to random port for testing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,24 +2,33 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
-
+    // Arrange
+    let url = spawn_app();
     let client = reqwest::Client::new();
-    client
-        .get("http://0.0.0.0:8000/health_check")
+
+    // Act
+    let response = client
+        .get(format!("{}/health_check", url))
         .send()
         .await
         .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
 #[allow(clippy::let_underscore_future)]
-fn spawn_app() {
+fn spawn_app() -> String {
     let app = zero2prod::app();
 
-    let listener = TcpListener::bind("0.0.0.0:8000").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
     let s = axum::Server::from_tcp(listener)
         .unwrap()
         .serve(app.into_make_service());
 
     let _ = tokio::spawn(s);
+    format!("http://127.0.0.1:{}", port)
 }
